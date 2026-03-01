ad597637bc601b1eb32bb1387d6888164c10e126
d-vuhong <Evilkun9x@gmail.com>
2026-03-01 17:21:20 +0900

    remove basePath

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/portfolio-VD', 
-  assetPrefix: '/portfolio-VD/', 
   eslint: {
     ignoreDuringBuilds: true,
   },
