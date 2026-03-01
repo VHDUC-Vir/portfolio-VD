c73120ec27453c098d87a0c8ed82f8bdd0a52ee1
d-vuhong <Evilkun9x@gmail.com>
2026-03-01 17:25:10 +0900

    rename out to docs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/portfolio-VD',
+  assetPrefix: '/portfolio-VD/',
   eslint: {
     ignoreDuringBuilds: true,
   },
