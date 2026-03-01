ab04b3284b96fb51327eff673dfa7f06674c5a84
d-vuhong <Evilkun9x@gmail.com>
2026-03-02 07:16:32 +0900

    fixErr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/portfolio-VD',
+  basePath: '/portfolio-VD', 
   assetPrefix: '/portfolio-VD/',
   eslint: {
     ignoreDuringBuilds: true,
@@ -8,3 +9,4 @@ const nextConfig = {
   images: { unoptimized: true },
 };
 
+module.exports = nextConfig;
